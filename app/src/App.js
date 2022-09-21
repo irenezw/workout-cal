c74bb884b254f0a1ee624a3e29a69ed4aed4c4dd
Irene Wang <ireneziwang@gmail.com>
2022-09-21 14:51:23 -0500

    create new react app

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Youtube Playlist Scheduler</h1>
+        <label for="url">Paste Youtube Playlist URL Here:</label>
+        <input type="url"></input>
+
+
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
